8ef9ac39d681e13f29ec14317d8f1e0598267102
Chris Estes <chestes@csumb.edu>
2021-02-04 13:52:25 -0800

    Created initial layout for create login page, updated strings.xml so fields/buttons don't use hardcoded strings.

---
+++ app/src/main/java/com/example/cst438_project1_group8/CreateLogin.java
@@ -0,0 +1,14 @@
+package com.example.cst438_project1_group8;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_login);
+    }
+}
