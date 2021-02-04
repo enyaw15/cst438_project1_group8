c29609fc148a73eccc45a83a7d9115f93ed0d493
fjhernan <60201738+fjhernan@users.noreply.github.com>
2021-02-04 13:14:07 -0800

    activity_layout created. Login button displays a toast.

--- app/src/main/java/com/example/cst438_project1_group8/LoginActivity.java
+++ app/src/main/java/com/example/cst438_project1_group8/LoginActivity.java
@@ -2,6 +2,9 @@ package com.example.cst438_project1_group8;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,5 +14,16 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         Log.d(LOGINACTIVITY, "onCreate called");
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_layout);
+
+        Button create_login_button = findViewById(R.id.login_button);
+        create_login_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(LOGINACTIVITY, "onClick called");
+                Toast toast = Toast.makeText(LoginActivity.this, "Login Toast", Toast.LENGTH_LONG);
+                toast.show();
+            }
+        });
     }
 }
