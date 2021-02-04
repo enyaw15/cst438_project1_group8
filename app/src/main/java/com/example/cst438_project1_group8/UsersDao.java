133e95efaee3205164ca040f3fa86b505657290f
enyaw15 <enyaw0215@gmail.com>
2021-02-04 13:53:05 -0800

    added users table via UsersDao, UserDatabase, User
    User is the table row
    UserDao contains quieres for the table
    UserDatabase allows creation of a usersdao so queries can be made

---
+++ app/src/main/java/com/example/cst438_project1_group8/UsersDao.java
@@ -0,0 +1,26 @@
+package com.example.cst438_project1_group8;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import java.util.List;
+
+@Dao
+public interface UsersDao {
+    @Query("SELECT * FROM Users")
+    List<User> getAll();
+
+    @Query("SELECT * FROM Users WHERE userID IN (:userIds)")
+    List<User> loadAllByIds(int[] userIds);
+
+    @Query("SELECT * FROM Users WHERE username LIKE :username LIMIT 1")
+    User findByName(String username);
+
+    @Insert
+    void insertAll(User... users);
+
+    @Delete
+    void delete(User user);
+}
