c70112d6d2a781f555a771671138025ad5d983c5
fjhernan <60201738+fjhernan@users.noreply.github.com>
2021-02-04 13:53:20 -0800

    LoginActivity nearly finished. Need to implement a check system.

--- app/src/main/java/com/example/cst438_project1_group8/LoginActivity.java
+++ app/src/main/java/com/example/cst438_project1_group8/LoginActivity.java
@@ -1,15 +1,18 @@
 package com.example.cst438_project1_group8;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
     public static final String LOGINACTIVITY = "LoginActivity";
+    public static final String USER = "User";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         Log.d(LOGINACTIVITY, "onCreate called");
@@ -23,6 +26,37 @@ public class LoginActivity extends AppCompatActivity {
                 Log.d(LOGINACTIVITY, "onClick called");
                 Toast toast = Toast.makeText(LoginActivity.this, "Login Toast", Toast.LENGTH_LONG);
                 toast.show();
+                EditText username = findViewById(R.id.username);
+                String name = username.getText().toString();
+                boolean userSuccess = false;
+                EditText password = findViewById(R.id.password);
+                String pword = password.getText().toString();
+                boolean passwordSuccess = false;
+                String msg = "";
+                boolean check = false;
+
+                //Implement code that checks for proper username and password here.
+
+
+                if(!(passwordSuccess)){
+                    msg += "Password is incorred.\n";
+                    check = true;
+                }
+                if(!(userSuccess)){
+                    msg += "User doesn't exist.\n";
+                    check = true;
+                }
+                if(!(check)){
+                    toast = Toast.makeText(LoginActivity.this, msg, Toast.LENGTH_LONG);
+                    toast.show();;
+                } else {
+                    Intent intent = new Intent();
+                    Bundle bundle = new Bundle();
+                    bundle.putString(USER, name);
+                    intent.putExtras(bundle);
+                    setResult(Activity.RESULT_OK, intent);
+                    finish();
+                }
             }
         });
     }
