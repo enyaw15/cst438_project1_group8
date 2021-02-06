a0ca0c7a63b046b090edf49e4ee2b8170efea6ac
enyaw15 <enyaw0215@gmail.com>
2021-02-05 22:16:34 -1000

    added singleton pattern to UserDatabase

--- app/src/main/java/com/example/cst438_project1_group8/UserDatabase.java
+++ app/src/main/java/com/example/cst438_project1_group8/UserDatabase.java
@@ -1,9 +1,23 @@
 package com.example.cst438_project1_group8;
 
+import android.content.Context;
+
 import androidx.room.Database;
+import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
 @Database(entities = {User.class}, version = 1)
 public abstract class UserDatabase extends RoomDatabase {
     public abstract UsersDao usersDao();
+
+    private static UserDatabase INSTANCE;
+
+    public static UserDatabase getUserDatabase(final Context context)
+    {
+        if(INSTANCE == null)
+        {
+            INSTANCE = Room.databaseBuilder(context.getApplicationContext(), UserDatabase.class, "users").build();
+        }
+        return INSTANCE;
+    }
 }
