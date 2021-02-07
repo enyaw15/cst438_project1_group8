8230cccfbe4a610f6c55cd3df7026fd006534e50
fjhernan <60201738+fjhernan@users.noreply.github.com>
2021-02-07 14:22:50 -0800

    Methods for checking username and password have been added. Cannot be used as of yet due to lack of room database. Will be updated once it is added.

--- app/src/main/java/com/example/cst438_project1_group8/LoginActivity.java
+++ app/src/main/java/com/example/cst438_project1_group8/LoginActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.util.Pair;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,7 +15,7 @@ import java.util.List;
 
 public class LoginActivity extends AppCompatActivity {
     public static final String LOGINACTIVITY = "LoginActivity";
-    public static final String USER = "User";
+    public static final String LOGIN = "Login";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         Log.d(LOGINACTIVITY, "onCreate called");
@@ -30,48 +31,68 @@ public class LoginActivity extends AppCompatActivity {
                 toast.show();
                 EditText username = findViewById(R.id.username);
                 String name = username.getText().toString();
-                boolean userSuccess = false;
+                Log.d(LOGINACTIVITY, "Username is " + name);
                 EditText password = findViewById(R.id.password);
                 String pword = password.getText().toString();
-                boolean passwordSuccess = false;
+                Log.d(LOGINACTIVITY, "Password is " + pword);
                 String msg = "";
                 boolean check = false;
 
                 //Implement code that checks for proper username and password here.
                 /*
                 List<userClass> users = dao.getRoom(LoginActivity.this).dao().getAllUsers();
-                for(int i = 0; i < users.size(); i++){
-                    if(name.equals(users.get(i).getUsername)){
-                        userSuccess = true;
-                        if(pword.equals(users.get(i).getPassword)){
-                            passwordSuccess = true;
-                            break; //User is correct, password is correct. End loop.
-                        } else {
-                            break; //User is correct, password is wrong. End loop
-                        }
+                Pair<Boolean, Integer> userCheck = checkForUser(name, users);
+                if(userCheck.getKey()){
+                    if(checkForPassword(pword, userCheck.getValue(), users)){
+                        check = true;
+                    } else {
+                        msg += "Password is incorrect.";
                     }
+                } else{
+                    msg = "Username is incorrect.\n";
                 }
                 */
-                    if(!(passwordSuccess)){
-                    msg += "Password is incorred.\n";
-                    check = true;
-                }
-                if(!(userSuccess)){
-                    msg += "User doesn't exist.\n";
-                    check = true;
-                }
                 if(!(check)){
                     toast = Toast.makeText(LoginActivity.this, msg, Toast.LENGTH_LONG);
                     toast.show();;
                 } else {
                     Intent intent = new Intent();
                     Bundle bundle = new Bundle();
-                    bundle.putString(USER, name);
+                    bundle.putString(LOGIN, name);
                     intent.putExtras(bundle);
                     setResult(Activity.RESULT_OK, intent);
                     finish();
                 }
             }
         });
+    }
+
+    /*
+    * Checks to see if user exists.
+    * Currently commented out due to no room database implementation yet.
+     */
+    public Pair<Boolean, Integer> checkForUser(String name/*,List<Users> users.*/){
+        /*
+        for(int i = 0; i < users.size(); i++){
+            if(name.equals(users.get(i).getUsername)){
+                userSuccess = true;
+                return new Pair<Boolean, Integer>(true, i);
+            }
+        }
+       */
+        return new Pair<Boolean, Integer>(false, 0);
+    }
+
+    /*
+    * Checks to see if password is correct for user.
+    * Currently comment out due to no room database implementation yet.
+     */
+    public boolean checkForPassword(String password, int index/*,List<Users> users.*/){
+        /*
+        if(users.get(index).getPassword.equals(password)){
+            return true;
+        }
+       */
+        return false;
     }
 }
