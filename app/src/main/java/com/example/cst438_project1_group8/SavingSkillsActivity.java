7845e0c5abd616b9978237e2a2d3de5b06a5af2a
fjhernan <60201738+fjhernan@users.noreply.github.com>
2021-02-13 15:27:03 -0800

    Made saving skills activity. Updated AndroidManifest.xml to include it.

---
+++ app/src/main/java/com/example/cst438_project1_group8/SavingSkillsActivity.java
@@ -0,0 +1,13 @@
+package com.example.cst438_project1_group8;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SavingSkillsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+    }
+}
